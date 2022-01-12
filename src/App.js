79d359aa74f1bdd4ebe908ddaba7580606d8141b
FlorentinaPetica <florentina.petica@gmail.com>
2022-01-12 14:26:58 +0100

    Initial portofolio setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Portofolio developing.
+        </p>
+    </div>
+  );
+}
+
+export default App;
